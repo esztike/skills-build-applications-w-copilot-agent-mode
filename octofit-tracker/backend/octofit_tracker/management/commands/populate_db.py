550b25620d47dc5e24de027dac57be043cb54600
Eszti Hollenback <56134483+esztike@users.noreply.github.com>
2025-04-08 20:49:00 +0000

    Add populate_db script and test data for Octofit app

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
-from octofit_tracker.test_data import test_data
+from octofit_tracker.sample_data import test_data
 from bson import ObjectId
 from datetime import timedelta
 
@@ -15,35 +15,49 @@ class Command(BaseCommand):
         Leaderboard.objects.all().delete()
         Workout.objects.all().delete()
 
-        # Create users
-        users = []
+        # Populate users
+        users = {}
         for user_data in test_data['users']:
-            user = User(_id=ObjectId(), **user_data)
-            user.save()
-            users.append(user)
+            user = User.objects.create(
+                _id=ObjectId(),
+                username=user_data['username'],
+                email=user_data['email'],
+                password=user_data['password']
+            )
+            users[user_data['username']] = user
 
-        # Create teams
+        # Populate teams
         for team_data in test_data['teams']:
-            team = Team(_id=ObjectId(), name=team_data['name'])
-            team.save()
+            team = Team.objects.create(
+                _id=ObjectId(),
+                name=team_data['name']
+            )
             for username in team_data['members']:
-                team.members.add(next(user for user in users if user.username == username))
+                team.members.add(users[username])
 
-        # Create activities
+        # Populate activities
         for activity_data in test_data['activities']:
-            user = next(user for user in users if user.username == activity_data['user'])
-            activity = Activity(_id=ObjectId(), user=user, activity_type=activity_data['activity_type'], duration=timedelta(hours=int(activity_data['duration'].split(':')[0]), minutes=int(activity_data['duration'].split(':')[1])))
-            activity.save()
+            Activity.objects.create(
+                _id=ObjectId(),
+                user=users[activity_data['user']],
+                activity_type=activity_data['activity_type'],
+                duration=timedelta(hours=int(activity_data['duration'].split(':')[0]), minutes=int(activity_data['duration'].split(':')[1]))
+            )
 
-        # Create leaderboard entries
+        # Populate leaderboard
         for leaderboard_data in test_data['leaderboard']:
-            user = next(user for user in users if user.username == leaderboard_data['user'])
-            leaderboard = Leaderboard(_id=ObjectId(), user=user, score=leaderboard_data['score'])
-            leaderboard.save()
+            Leaderboard.objects.create(
+                _id=ObjectId(),
+                user=users[leaderboard_data['user']],
+                score=leaderboard_data['score']
+            )
 
-        # Create workouts
+        # Populate workouts
         for workout_data in test_data['workouts']:
-            workout = Workout(_id=ObjectId(), **workout_data)
-            workout.save()
+            Workout.objects.create(
+                _id=ObjectId(),
+                name=workout_data['name'],
+                description=workout_data['description']
+            )
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
